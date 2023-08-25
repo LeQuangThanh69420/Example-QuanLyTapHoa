ed5dfb5131a35fe374b4083e1947592d18fbac67
LeeHaii <93501285+LeeHaii@users.noreply.github.com>
2023-08-25 22:39:24 +0700

    omg omg

--- back/Controllers/BaseApiController.cs
+++ back/Controllers/BaseApiController.cs
@@ -2,12 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using back.datacontext;
+using back.Entity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace back.Controllers
 {
+    [ApiController]
+    [Route("api")]
     public class BaseApiController : ControllerBase
     {
-        
+        private readonly DataContext _context; 
+        public BaseApiController(DataContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet("getCategory")]
+        public ActionResult<List<Category>> getCategory(){ //generic
+            var category = _context.Category.AsQueryable().Where(c=>c.IsActive == true); // c duoc lay tu thg Category o doan _context.Category
+            return category.ToList();
+        }
+        [HttpGet("getSanPham")]
+        public ActionResult<List<SanPham>> getSanPham(){ //generic
+            var sanPham = _context.SanPham.AsQueryable(); 
+            return sanPham.ToList();
+        }
     }
 }
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
-
-  }
+  constructor(private http: HttpClient) {}
+  getCategories:any;
   ngOnInit() {
+    this.http.get("http://localhost:5235/api/getCategory").subscribe( response => { // response la luu dong du lieu cua ham get
+      this.getCategories = response;
+    });
 
+  
   }
+  
+  apiService: ApiService;
   
 }
